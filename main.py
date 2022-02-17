81e08d21584b8d6731e78b8dea36bee5ce7095c5
Ecem Sogancioglu <ecem.lago@radboudumc.nl>
2022-02-17 12:14:45 +0100

    added failed test

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def sum(a,b):
     return a+b
 
 def subtract(a,b):
-    return a-b
+    return a+b
 
 
 def test_sum():
     assert sum(2,3) == 5
     assert sum(8,2) == 10
+
+# uncomment the following test in step 5
+def test_subtract():
+    assert subtract(2, 3) == -1
